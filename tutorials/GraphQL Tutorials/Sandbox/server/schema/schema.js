ce9b0e5e1b17f2f29a4586c1fc15e688d0e527a4
Learn.Devcat <learn.devcat@outlook.com>
2018-09-10 08:30:15 -0500

    New Tutorial for GraphQL
    Lesson modules

---
+++ tutorials/GraphQL Tutorials/Sandbox/server/schema/schema.js
@@ -0,0 +1,43 @@
+const graphql = require('graphql');
+const_ = require('lodash');
+
+const { GraphQLObejctType, GraphQLString, GraphQLSchema } = graphql;
+
+
+// dummy data
+var books = [
+    { name: 'Name of the Wind',genre: 'Fantasy',id: '1' },
+    { name: 'The Final Empire',genre: 'Fantasy',id: '2' },
+    { name: 'The Long Earth',genre: 'Sci-Fi',id: '3'}
+];
+
+const BookType = new GraphQLObejctType({
+    name: 'Book',
+    fields: () => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString},
+        genre: { type: GraphQLString }
+    })
+});
+
+
+const RootQuery = new GraphQLObejctType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: { id: { type: GraphQLString } },
+            resolve(parent,args){
+                // code to get data from db / other source
+                return_.find(books,{id:args.id});
+            }
+        }
+    }
+});
+
+
+
+
+export default new GraphQLSchema({
+    query: RootQuery
+});
