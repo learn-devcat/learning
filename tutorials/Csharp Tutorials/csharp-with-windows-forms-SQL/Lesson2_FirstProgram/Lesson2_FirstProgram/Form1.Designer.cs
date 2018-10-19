1bde1b47c5f65178fef74064c9fd6c996dfa16b1
Learn.Devcat <learn.devcat@outlook.com>
2018-10-19 09:24:59 -0500

    Friday Commits

---
+++ tutorials/Csharp Tutorials/csharp-with-windows-forms-SQL/Lesson2_FirstProgram/Lesson2_FirstProgram/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Lesson2_FirstProgram
+{
+   partial class Form1
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose( bool disposing )
+      {
+         if ( disposing && (components != null) )
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.msgLabel = new System.Windows.Forms.Label();
+         this.SuspendLayout();
+         // 
+         // msgLabel
+         // 
+         this.msgLabel.AutoSize = true;
+         this.msgLabel.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.msgLabel.Location = new System.Drawing.Point(63, 81);
+         this.msgLabel.Name = "msgLabel";
+         this.msgLabel.Size = new System.Drawing.Size(0, 45);
+         this.msgLabel.TabIndex = 0;
+         // 
+         // Form1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+         this.ClientSize = new System.Drawing.Size(991, 598);
+         this.Controls.Add(this.msgLabel);
+         this.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+         this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+         this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+         this.Name = "Form1";
+         this.Text = "My Form";
+         this.Load += new System.EventHandler(this.Form1_Load);
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.Label msgLabel;
+   }
+}
+
