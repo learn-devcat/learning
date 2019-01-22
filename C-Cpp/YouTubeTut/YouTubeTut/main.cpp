32c3b5d35ed5fc89bf1b60d8f3083b71123abc84
Learn.Devcat <learn.devcat@outlook.com>
2019-01-22 08:21:16 -0600

    learning sync 22

---
+++ C-Cpp/YouTubeTut/YouTubeTut/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void printScreen(int x);
+
+
+int main()
+{
+    printScreen(27);
+
+    return 0;
+}
+
+
+
+void printScreen(int x) {
+    cout << "My favorite number is " << x << endl;
+}
+
---
+++ math/Math2.py
@@ -0,0 +1,4 @@
+from sympy import * as sp
+
+
+
